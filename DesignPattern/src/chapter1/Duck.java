bf61c71123df2c1e9a8f705a344a20c69baa57c4
加藤大陽 <taiyo.kato.1209@gmail.com>
2014-09-07 16:37:05 +0900

    Duckクラスの実装。飛ぶ動作や鳴く動作は委譲して変更に強くする。

--- DesignPattern/src/chapter1/Duck.java
+++ DesignPattern/src/chapter1/Duck.java
@@ -2,4 +2,24 @@ package chapter1;
 
 public abstract class Duck {
 
+	FlyBehavior flyBehavior;
+	QuackBehavior quackBehavior;
+	
+	public Duck() {
+		
+	}
+	
+	public abstract void displya();
+	
+	public void performFly() {
+		flyBehavior.fly();
+	}
+	
+	public void performQuack() {
+		quackBehavior.quack();
+	}
+	
+	public void swim() {
+		System.out.println("すべての鴨は浮かびます。おとりの鴨でも！");
+	}
 }
