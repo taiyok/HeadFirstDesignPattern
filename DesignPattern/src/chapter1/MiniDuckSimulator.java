68403a9504736e9436081fd363f92b4aeea509fe
加藤大陽 <taiyo.kato.1209@gmail.com>
2014-09-07 19:35:37 +0900

    鳴き声を実装し、実際にテスト

--- DesignPattern/src/chapter1/MiniDuckSimulator.java
+++ DesignPattern/src/chapter1/MiniDuckSimulator.java
@@ -2,4 +2,9 @@ package chapter1;
 
 public class MiniDuckSimulator {
 
+	public static void main(String[] args) {
+		Duck mallard = new MallardDuck();
+		mallard.performQuack();
+		mallard.performFly();
+	}
 }
--- DesignPattern/src/chapter1/Sqeak.java
+++ DesignPattern/src/chapter1/Sqeak.java
@@ -4,8 +4,7 @@ public class Sqeak implements QuackBehavior {
 
 	@Override
 	public void quack() {
-		// TODO 自動生成されたメソッド・スタブ
-
+		System.out.println("キューキュー");
 	}
 
 }
